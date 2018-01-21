f75d9b5606aff8018c5c17c865433b9b7f412ee2
djb77 <dwayne.bakewell@gmail.com>
2018-01-21 11:49:35 +1100

    fs: Update F2FS from @jaegeuk f2fs-stable v4.16-rc1-3.18
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- fs/sync.c
+++ fs/sync.c
@@ -445,6 +445,7 @@ SYSCALL_DEFINE1(syncfs, int, fd)
  */
 int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
+
 	if (!file->f_op->fsync)
 		return -EINVAL;
 	return file->f_op->fsync(file, start, end, datasync);
